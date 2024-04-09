818972d7811263fe6a823ee1695f1c01092b6df9
ion-project <gabrielcursino.medeiros@gmail.com>
2024-04-09 18:45:17 -0300

    feat: dynamic mask, closes #2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import { Mask } from '@ionited/mask';
 
 export function mask(node: HTMLInputElement, parameters?: any) {
-  const mask = Mask(node, parameters);
+  let mask: any;
+
+  if (parameters) mask = Mask(node, parameters);
 
   return {
+    update(parameters?: any) {
+      mask?.instance.destroy();
+      
+      if (parameters) mask = Mask(node, parameters);
+    },
+
     destroy() {
-      mask.instance.destroy();
+      mask?.instance.destroy();
     }
   }
 }
